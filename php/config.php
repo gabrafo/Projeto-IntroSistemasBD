f59b93663fa3b325cdca9b248ab140f0f66530cf
Gabriel Fagundes <113377700+gabrafo@users.noreply.github.com>
2025-02-04 17:57:48 -0300

    adicionando doc

--- php/config.php
+++ php/config.php
@@ -1,6 +1,18 @@
 <?php
-    $host = "127.0.0.1";
+    // Configurações de conexão com o banco de dados
+    
+    // Endereço do servidor MySQL (normalmente 'localhost' em ambientes locais)
+    $host = "localhost";
+    
+    // Usuário do banco de dados (padrão XAMPP/WAMP é 'root')
     $login = "root";
+    
+    // Senha do banco de dados (vazia por padrão em ambientes de desenvolvimento)
     $senha = "";
+    
+    // Nome do banco de dados que será utilizado
     $bd = "projetoISBD";
+    
+    // Estas variáveis são usadas posteriormente para estabelecer a conexão MySQLi
+    // Exemplo de uso: new mysqli($host, $login, $senha, $bd);
 ?>
