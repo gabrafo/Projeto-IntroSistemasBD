103d6d980ece8f2bf33c5c8332a7d8cbe2929329
Gabriel Fagundes <113377700+gabrafo@users.noreply.github.com>
2025-02-04 18:07:54 -0300

    adicionando doc

--- php/index.php
+++ php/index.php
@@ -1,18 +1,19 @@
 <?php
-header("Content-Type: text/html; charset=utf-8", true);
+    // Define cabeçalho com codificação UTF-8 para caracteres especiais
+    header("Content-Type: text/html; charset=utf-8", true);
 ?>
 <html>
-
 <head>
-  <title>Personagens Jogáveis</title>
+    <title>Personagens Jogáveis</title>
 </head>
-
 <body>
   <center>
     <h3>Lista de Personagens Jogáveis</h3>
   </center>
+  <!-- Formulário principal com botão de inclusão -->
   <form name="form1" method="POST" action="form_incluir.php">
     <table border="1" align="center" width="100%">
+      <!-- Cabeçalho da tabela -->
       <tr bgcolor="grey">
         <td>Nome</td>
         <td>Idade</td>
@@ -29,20 +30,32 @@ header("Content-Type: text/html; charset=utf-8", true);
         <td>Ações</td>
       </tr>
       <?php
+      // Conexão com banco de dados
       include("./config.php");
       $con = mysqli_connect($host, $login, $senha, $bd);
-      $sql = "SELECT Jogavel.*, Classe.tipoClasse FROM Jogavel JOIN Classe ON Jogavel.idClasse = Classe.idClasse ORDER BY nome";
+      
+      // Query que combina dados do personagem com sua classe
+      $sql = "SELECT Jogavel.*, Classe.tipoClasse FROM Jogavel 
+              JOIN Classe ON Jogavel.idClasse = Classe.idClasse 
+              ORDER BY nome";
+              
       $tabela = mysqli_query($con, $sql);
+      
+      // Verifica se há registros retornados
       if (mysqli_num_rows($tabela) == 0) {
-        ?>
+      ?>
+        <!-- Mensagem para lista vazia -->
         <tr>
           <td colspan="13" align="center">Nenhum personagem cadastrado.</td>
         </tr>
-        <?php
+      <?php
       } else {
+        // Loop através de cada registro do banco
         while ($dados = mysqli_fetch_assoc($tabela)) {
-          ?>
+      ?>
+          <!-- Linha da tabela para cada personagem -->
           <tr>
+            <!-- Exibição dos dados -->
             <td><?php echo $dados['nome']; ?></td>
             <td><?php echo $dados['idade']; ?></td>
             <td><?php echo $dados['raca']; ?></td>
@@ -56,22 +69,24 @@ header("Content-Type: text/html; charset=utf-8", true);
             <td><?php echo $dados['dano']; ?></td>
             <td><?php echo $dados['tipoClasse']; ?></td>
             <td>
-              <input type="button" value="Excluir"
+              <input type="button" value="Excluir" 
                 onclick="location.href='excluir.php?idPersonagem=<?php echo $dados['idPersonagem']; ?>'">
-              <input type="button" value="Editar"
+              <input type="button" value="Editar" 
                 onclick="location.href='form_incluir.php?idPersonagem=<?php echo $dados['idPersonagem']; ?>'">
             </td>
           </tr>
-          <?php
+      <?php
         }
       }
       mysqli_close($con);
       ?>
+      <!-- Botão para inclusão de novo personagem -->
       <tr>
-        <td colspan="13" align="center"><input type="submit" value="Incluir Novo Personagem"></td>
+        <td colspan="13" align="center">
+          <input type="submit" value="Incluir Novo Personagem">
+        </td>
       </tr>
     </table>
   </form>
 </body>
-
 </html>
